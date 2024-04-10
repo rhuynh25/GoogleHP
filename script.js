cd3d65d8221b3fff7390d5d1ebeb16ea8f3669bd
Richard Huynh <rhuynh125@gmail.com>
2024-04-10 16:23:19 -0400

    update commit

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-function googleSearch(){
-    window.location = "https://www.google.com/search?q=" + document.getElementById("search").value
-  }
-  
-    // window.open location ("https://www.google.com/search?q=" + document.querySelector("textarea").value)
-  
-  function goAdvancedSearch() {
-    window.location.href = "https://www.google.com/advanced_image_search?hl=en&fg=1";
-  }
-  
-  function goImageSearch() {
-    window.location.href = "https://www.google.com/imghp?hl=en&authuser=0&ogbl";
-  }
+function googleSearch() {
+  event.preventDefault()
+  window.location = "https://www.google.com/search?q=" + document.getElementById("search").value
+}
+
+function goImageSearch() {
+  window.location = "https://www.google.com/search?tbm=isch&q=" + document.getElementById("search").value
+}
+
+// function goAdvancedSearch() {
+//   window. location = "https://www.google.com/advanced_image_search?hl=en&fg=1";
+// }
+
+
+// window.open location ("https://www.google.com/search?q=" + document.querySelector("textarea").value)
